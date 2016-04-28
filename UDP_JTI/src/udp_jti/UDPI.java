2e4c7f90927c4c8f8882cca85fceddcbc8adb869
Kirial <jesperkirial@gmail.com>
2016-04-28 14:19:34 +0200

    Jesper start på buffer og send

--- UDP_JTI/src/udp_jti/UDPI.java
+++ UDP_JTI/src/udp_jti/UDPI.java
@@ -2,9 +2,11 @@ package udp_jti;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Scanner;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -16,15 +18,21 @@ import java.util.logging.Logger;
  * @author Family
  */
 public class UDPI {
+
     private byte[] receiveD;
     private byte[] send;
     private String connection;
     private int PORT_NR;
-    private String IP;
+    //private String IP;
     private DatagramSocket socket = null;
     private String thisMessage;
     private UDPII target;
     private String dataType;
+    private static String inData;
+    private ArrayList<String> toBeSent;
+    private DatagramSocket clientSocket;
+    private byte[] sendData;
+    InetAddress IP;
 
     public UDPI(UDPII yourCode, int p) throws Exception {
         PORT_NR = p;
@@ -60,7 +68,7 @@ public class UDPI {
         int newPort;
         String connect = "Transmission.";
         connect = connect + dataType;
-        
+
         byte[] dataB = connect.getBytes();
         try {
             InetAddress address = InetAddress.getByName(adr);
@@ -70,18 +78,20 @@ public class UDPI {
                 byte[] tempData = new byte[60];
                 DatagramPacket receivePacket = new DatagramPacket(tempData, tempData.length);
                 socket.receive(receivePacket);
-                String con = new String(receivePacket.getData()); 
-                String newPortS = con.substring(con.indexOf('.'),con.length());
+                String con = new String(receivePacket.getData());
+                String newPortS = con.substring(con.indexOf('.'), con.length());
                 newPort = Integer.parseInt(newPortS);
+                Scanner keyboard = new Scanner(System.in);
+                inData = keyboard.nextLine();
+                System.out.println("enter IP Address");
+                IP = keyboard.nextLine();
+                ArrayList<String> toBeSent = new ArrayList<String>();
+                buffer();
                 
-                // skal skrives lidt kode 
-                
-                
+
             } catch (IOException ex) {
                 System.out.println(Arrays.toString(ex.getStackTrace()));
             }
-            
-            
 
         } catch (UnknownHostException ex) {
             System.out.println("Invalid IP Adress");
@@ -128,4 +138,22 @@ public class UDPI {
 
     }
 
-}
+    private void buffer() {
+                    while (inData.length() > 512) {
+                toBeSent.add(inData.substring(0, 511));
+                inData = inData.substring(512);
+            }
+            toBeSent.add(inData);
+        }
+       
+    private void Sending() throws IOException {
+        for (String temp : toBeSent){
+         sendData = temp.getBytes();
+            
+            DatagramPacket sendPacket =  new DatagramPacket(sendData, sendData.length, IP, 9876);
+          clientSocket.send(sendPacket);
+            
+        } 
+    }
+    }
+
