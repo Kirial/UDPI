5f27eae5fc346d11557f1528d3f50eff1d37cca4
Kirial <jesperkirial@gmail.com>
2016-04-28 17:25:26 +0200

    Tror det virker

--- UDP_JTI/src/udp_jti/UDPI.java
+++ UDP_JTI/src/udp_jti/UDPI.java
@@ -36,6 +36,8 @@ public class UDPI {
     InetAddress IP;
     int portnr;
     String adr;
+    private ArrayList<String> missingPackets;
+    int runde = 0;
 
     /**
      * This constructor creates a port that will listen to the port
@@ -99,7 +101,7 @@ public class UDPI {
                 Scanner keyboard = new Scanner(System.in);
                 inData = keyboard.nextLine();
                 buffer();
-                sendOrder();
+                sendBurst();
                 waitAck();
 
             } catch (IOException ex) {
@@ -152,35 +154,95 @@ public class UDPI {
     }
 
     /**
-     * This method will devide the inData into chunks of 512 bytes
+     * This method will divide the inData into chunks of 512 bytes
      */
     private void buffer() {
-        while (inData.length() > 512) {
-            toBeSent.add(inData.substring(0, 511));
-            inData = inData.substring(512);
+        while (inData.length() > 487) {
+            toBeSent.add(inData.substring(0, 486));
+            inData = inData.substring(487);
         }
         toBeSent.add(inData);
     }
 
-    private void sendOrder() throws UnknownHostException, IOException {
-        int runde = 0;
-        for (int i = 0; i < 100; i++) {
+    private void sendBurst() throws UnknownHostException, IOException {
+        int count = 0;
+        while (count < 100 && count + runde < toBeSent.size()) {
+            int antal = inData.length() % 487;
+            int flere = toBeSent.size() - (runde);
 
-            sendData = toBeSent.get(i + runde);
+            int S = 1;
+            if (flere > 0) {
+                S = 1;
+            } else {
+                S = 0;
+            }
+
+            int nummer = 0;
+            String header = ("HEAD*A" + antal + "#" + nummer + "S" + S + "*HEAD");
+            String data = toBeSent.get(count + runde);
+            String headerMedData = header + data;
+            nummer++;
+            sendData = headerMedData;
             sendDataBytes = sendData.getBytes();
             DatagramPacket sendPacket = new DatagramPacket(sendDataBytes, sendDataBytes.length, InetAddress.getByName(adr), portnr);
             clientSocket.send(sendPacket);
-            if (i == 99) {
-                runde = runde + 100;
-            }
+            count++;
         }
     }
 
+    /**
+     * Metode til at sende pakker enkeltvis hvis de bliver angivet i en
+     * acknowledgement
+     *
+     * @param missing
+     * @throws UnknownHostException
+     * @throws IOException
+     */
+    private void sendSingle(int missing) throws UnknownHostException, IOException {
+
+        sendData = toBeSent.get(missing);
+        sendDataBytes = sendData.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendDataBytes, sendDataBytes.length, InetAddress.getByName(adr), portnr);
+        clientSocket.send(sendPacket);
+        waitAck();
+    }
+
     private void waitAck() {
+
+        DatagramPacket receivePacket = new DatagramPacket(receiveD, receiveD.length);
         try {
             clientSocket.setSoTimeout(20);
-        } catch (SocketException ex) {
+            clientSocket.receive(receivePacket);
+            String Ack = new String(receivePacket.getData());
+            ProcessAck(Ack);
+            clientSocket.close();
+
+        } catch (SocketTimeoutException timeout) {
+
+        } catch (IOException ex) {
             Logger.getLogger(UDPI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void ProcessAck(String Ack) {
+
+        String modtagetOK = Ack.substring(Ack.indexOf('K') + 1, Ack.indexOf('N'));
+        String missingstring = Ack.substring(Ack.indexOf('t'));
+        int missing = Integer.parseInt(missingstring);
+
+        if (missing == 99) {
+            runde = runde + 100;
+            try {
+                sendBurst();
+            } catch (IOException ex) {
+                Logger.getLogger(UDPI.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            try {
+                sendSingle(missing);
+            } catch (IOException ex) {
+                Logger.getLogger(UDPI.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 }
