6629da2fa811d594c2263542923b7c590f94b7d8
taras <taras@10.16.166.249>
2016-04-28 17:36:48 +0200

    Jesper Change

--- UDP_JTI/src/udp_jti/UDPI.java
+++ UDP_JTI/src/udp_jti/UDPI.java
@@ -38,6 +38,10 @@ public class UDPI {
     String adr;
     private boolean listen;
 
+    private ArrayList<String> missingPackets;
+    int runde = 0;
+
+
     /**
      * This constructor creates a port that will listen to the port
      * <p>
@@ -105,9 +109,14 @@ public class UDPI {
                 newPort = Integer.parseInt(newPortS);
                 Scanner keyboard = new Scanner(System.in);
                 inData = keyboard.nextLine();
+
                 ArrayList<String> toBeSent = new ArrayList<String>();
                 buffer();
                 sendOrder();
+
+                buffer();
+                sendBurst();
+                waitAck();
 
             } catch (IOException ex) {
                 System.out.println(Arrays.toString(ex.getStackTrace()));
@@ -145,11 +154,9 @@ public class UDPI {
                                 try {
                                     new Thread(new serverStart(newSocket, target, IPAddress, dataType)).start();
                                 } catch (Exception ex) {
-                                    System.out.println("TESTST¤SGSGSDGGSD");
                                 }
                                 break;
                             } catch (SocketException e) {
-                                System.out.println("AAAAAAAAAAAA");
                                 newPort++;
                                 if (newPort >= 65535) {
                                     System.out.println("Error");
@@ -174,13 +181,6 @@ public class UDPI {
     /**
      * This method will devide the inData into chunks of 512 bytes
      */
-    private void buffer() {
-        while (inData.length() > 512) {
-            toBeSent.add(inData.substring(0, 511));
-            inData = inData.substring(512);
-        }
-        toBeSent.add(inData);
-    }
 
     private void sendOrder() throws UnknownHostException, IOException {
         int runde = 0;
@@ -195,7 +195,98 @@ public class UDPI {
             }
         }
         socket.setSoTimeout(20);
+    }
+    /**
+     * This method will divide the inData into chunks of 512 bytes
+     */
+    private void buffer(){
+        while (inData.length() > 487) {
+            toBeSent.add(inData.substring(0, 486));
+            inData = inData.substring(487);
+        }
+        toBeSent.add(inData);
+    }
 
+    private void sendBurst() throws UnknownHostException, IOException {
+        int count = 0;
+        while (count < 100 && count + runde < toBeSent.size()) {
+            int antal = inData.length() % 487;
+            int flere = toBeSent.size() - (runde);
+
+            int S = 1;
+            if (flere > 0) {
+                S = 1;
+            } else {
+                S = 0;
+            }
+
+            int nummer = 0;
+            String header = ("HEAD*A" + antal + "#" + nummer + "S" + S + "*HEAD");
+            String data = toBeSent.get(count + runde);
+            String headerMedData = header + data;
+            nummer++;
+            sendData = headerMedData;
+            sendDataBytes = sendData.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendDataBytes, sendDataBytes.length, InetAddress.getByName(adr), portnr);
+            clientSocket.send(sendPacket);
+            count++;
+        }
+    }
+
+    /**
+     * Metode til at sende pakker enkeltvis hvis de bliver angivet i en
+     * acknowledgement
+     *
+     * @param missing
+     * @throws UnknownHostException
+     * @throws IOException
+     */
+    private void sendSingle(int missing) throws UnknownHostException, IOException {
+
+        sendData = toBeSent.get(missing);
+        sendDataBytes = sendData.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendDataBytes, sendDataBytes.length, InetAddress.getByName(adr), portnr);
+        clientSocket.send(sendPacket);
+        waitAck();
+    }
+
+    private void waitAck() {
+
+        DatagramPacket receivePacket = new DatagramPacket(receiveD, receiveD.length);
+        try {
+            clientSocket.setSoTimeout(20);
+            clientSocket.receive(receivePacket);
+            String Ack = new String(receivePacket.getData());
+            ProcessAck(Ack);
+            clientSocket.close();
+
+        } catch (SocketTimeoutException timeout) {
+
+        } catch (IOException ex) {
+            Logger.getLogger(UDPI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void ProcessAck(String Ack) {
+
+        String modtagetOK = Ack.substring(Ack.indexOf('K') + 1, Ack.indexOf('N'));
+        String missingstring = Ack.substring(Ack.indexOf('t'));
+        int missing = Integer.parseInt(missingstring);
+
+        if (missing == 99) {
+            runde = runde + 100;
+            try {
+                sendBurst();
+            } catch (IOException ex) {
+                Logger.getLogger(UDPI.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            try {
+                sendSingle(missing);
+            } catch (IOException ex) {
+                Logger.getLogger(UDPI.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 }
 
